3d92c3d5472efda7f012a34d6d70b00f40b6c164
Juan Aceves <juanaceves.ang98gmail.com>
2023-08-21 19:19:13 -0600

    print added

--- Practicas/practicas.py
+++ Practicas/practicas.py
@@ -1,2 +1,8 @@
 #All practices are on next url
 ## http://localhost:8888/notebooks/anaconda3/Colab%20Notebooks/Practicas.ipynb# 
+
+
+#Imprimir la frase
+print('''La felicidad se puede encontrar hasta en los mas oscuros momentos, 
+si somos capaces d eusar bien la luz.
+ - Albus Dumbledore''')
